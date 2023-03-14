0432a9448a2e97b49671db8bcf941faeb74275e9
Matthew <matthew@digital-thought.org>
2023-03-15 10:34:23 +1100

    Initial commit

---
+++ src/dtWebToolkitFramework/_version.py
@@ -0,0 +1,8 @@
+_VERSION = '1.0.0a1'
+_MAINTAINER = 'Matthew @ Digital-Thought'
+_AUTHOR = 'Digital-Thought'
+_AUTHOR_EMAIL = 'matthew@digital-thought.org'
+_DESCRIPTION = "Framework for making Digital-Thought Web Toolkit"
+_TITLE = "dtWebToolkitFramework"
+_PROJECT_URL = ""
+_DOCS_URL = ""
